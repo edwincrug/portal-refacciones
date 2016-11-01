b06af0a72c2647c897ecb9ca972627c4456607db
bigpada <anta0101@hotmail.com>
2016-10-31 18:36:24 -0600

    pedido modal

--- client/app/user/pedido/modalPedido/modalPedido.router.js
+++ client/app/user/pedido/modalPedido/modalPedido.router.js
@@ -18,7 +18,7 @@ class ModalPedidoComponent {
             console.log($stateParams.idcolor)
             $scope.colorEstatus = '#' + $stateParams.idcolor
             $scope.estatus = $stateParams.estatus
-            //console.log($stateParams.idcolor)
+                //console.log($stateParams.idcolor)
             Pedido.get({
                 id: $stateParams.id,
                 user: $scope.user.per_idpersona,
@@ -29,11 +29,11 @@ class ModalPedidoComponent {
                 $scope.detalles = data.data;
                 $scope.empresa = data;
 
-                var i=0;
-                $scope.subtotal=0;
+                var i = 0;
+                $scope.subtotal = 0;
                 angular.forEach($scope.detalles, function(value, key) {
-                  $scope.subtotal+= $scope.detalles[i].totalItem;
-                  i++;
+                    $scope.subtotal += $scope.detalles[i].totalItem;
+                    i++;
                 });
 
                 $scope.idpedido = $stateParams.idpedido;
@@ -50,12 +50,14 @@ class ModalPedidoComponent {
             console.log('empty')
         }
 
-        $scope.imprimir = function() {          
+        $scope.imprimir = function() {
 
-          var objeto=document.getElementById('imprimeme');  //obtenemos el objeto a imprimir
 
-          var ventana = window.open('', 'my div', 'height=400,width=600');//window.open('','_blank');  //abrimos una ventana vacía nueva
-           //var mywindow = window.open('', 'my div', 'height=400,width=600');
+
+            var objeto = document.getElementById('imprimeme'); //obtenemos el objeto a imprimir
+
+            var ventana = window.open('', 'my div', 'height=400,width=600'); //window.open('','_blank');  //abrimos una ventana vacía nueva
+            //var mywindow = window.open('', 'my div', 'height=400,width=600');
 
             /*var style = ventana.document.createElement('link');
             style.type = "text/css";
@@ -68,9 +70,9 @@ class ModalPedidoComponent {
 
             ventana.document.write('<style type="text/css">');
             //ventana.document.write('body {margin:0; padding:0; line-height: 1.4em; word-spacing:1px; letter-spacing:0.2px; font: 10px Arial, Helvetica,"Lucida Grande", serif; color: #000;}');
-            ventana.document.write('.label {background-color: #389DF5;}');
+            ventana.document.write('.label2 {background-color: #389DF5;}');
             ventana.document.write('</style>');
-            
+
             /*ventana.document.write('<link rel="stylesheet" href="bower_components/components-font-awesome/css/font-awesome.css" />');
             ventana.document.write('<link rel="stylesheet" href="bower_components/bootstrap-table/src/bootstrap-table.css" />');
             
@@ -78,18 +80,18 @@ class ModalPedidoComponent {
             ventana.document.write('<link rel="stylesheet" href="components/portal.css" />');
             ventana.document.write('<link rel="stylesheet" href="components/style.css" />');*/
             //ventana.document.write('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />');
-            
+
             //ventana.document.write("<link rel='stylesheet' href='app/user/pedido/pedido.css' />");            
-            
+
             //console.log(objeto.innerHTML);
 
-            ventana.document.write('</head><body>');
-            ventana.document.write(objeto.innerHTML);   //imprimimos el HTML del objeto en la nueva ventana
+            ventana.document.write('</head><body>');            
+            ventana.document.write(objeto.innerHTML); //imprimimos el HTML del objeto en la nueva ventana
             ventana.document.write('</body></html>');
 
             //ventana.document.getElementsByTagName("head")[0].appendChild(style);
 
-            ventana.document.close();  //cerramos el documento
+            ventana.document.close(); //cerramos el documento
             ventana.print();  //imprimimos la ventana
             ventana.close();  //cerramos la ventana
         }
